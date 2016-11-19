1da84d86184df973dfcb10974116cae1f2f8b3f3
alhimik45 <alhimik45@gmail.com>
2016-11-19 23:15:43 +0700

    fix song output

--- better-status.sh
+++ better-status.sh
@@ -1,15 +1,17 @@
 #!/bin/sh
 
 :> /dev/shm/fifo-player-song
+:> /dev/shm/better-status-line
 
 tail -f /dev/shm/fifo-player-song 2>/dev/null | while read line
 do
-	killall -USR1 i3status
+	echo "$(cat /dev/shm/fifo-player-song ) | Micro: $(amixer get Capture | grep "Front Left"|tail -1| grep -E -o "on|off" | tail -1) |$(cat /dev/shm/better-status-line)"
 done &
 
 sleep 1 && killall -USR1 i3status &
 i3status --config ~/.i3/i3status.conf | while :
 do
         read line
+        echo $line > /dev/shm/better-status-line
         echo "$(cat /dev/shm/fifo-player-song ) | Micro: $(amixer get Capture | grep "Front Left"|tail -1| grep -E -o "on|off" | tail -1) |$line" || exit 1
 done
